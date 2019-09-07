764fe66513e6c40a698f7618b5f086f537fb0e43
mikesaurio <mikesaurio@gmail.com>
2019-09-07 13:00:58 -0500

    devise y su configiracion

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+    devise_for :users , controllers:{passwords: 'users/passwords', sessions: 'users/sessions'}
+    root to: 'static_pages#index'
+  end
 end
