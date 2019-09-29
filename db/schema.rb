d7c8bef6e08b0ed4ce94150f6b5fea47beabbc7d
mikesaurio <mikesaurio@gmail.com>
2019-09-29 13:10:26 -0500

    add searcher

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_07_225414) do
+ActiveRecord::Schema.define(version: 2019_09_29_152756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2019_09_07_225414) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_complains_on_email"
     t.index ["name"], name: "index_complains_on_name"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
