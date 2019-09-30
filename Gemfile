bcbf71a4d7b503ef34b7ef84cdb4d5434eea70b5
mikesaurio <mikesaurio@gmail.com>
2019-09-29 22:10:47 -0500

    dinamic questions

--- Gemfile
+++ Gemfile
@@ -39,12 +39,13 @@ gem 'kaminari'
 
 gem 'pg_search'
 gem 'toastr-rails'
+gem 'ffaker'
 
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'ffaker'
+
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'ffaker'
 if Rails.env.development?
-  User.create(email: "tester@data.uy", password: "pass_4321", password_confirmation: 'pass_4321')
+  #User.create(email: "tester@data.uy", password: "pass_4321", password_confirmation: 'pass_4321')
 
   50.times do
     Question.create({title: FFaker::AnimalUS.common_name, school_type: Question.school_types.values.sample})
