78c6c565788918cfa8dd54b563792566cc793dc7
mikesaurio <mikesaurio@gmail.com>
2019-10-17 08:05:29 -0500

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,19 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings
+  config.action_mailer.smtp_settings = {
+  :address              => "freyja.datauy.org",
+  :port                 => 587,
+  :domain               => Rails.application.credentials[:domain],
+  :user_name            => Rails.application.credentials[:mail_username],
+  :password             => Rails.application.credentials[:mail_password],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'hola@derechos_de_estudiantes.uy'
+  config.mailer_sender = 'contacto@derechosdeestudiantes.edu.uy'
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
